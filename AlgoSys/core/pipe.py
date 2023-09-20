10b1ee91dc651016bfaef185afa41d07d0da6aef
Jay <jj830502@gmail.com>
2023-09-20 21:29:33 +0800

    pipeline

---
+++ AlgoSys/core/pipe.py
@@ -0,0 +1,11 @@
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import (
+    StandardScaler,
+    MinMaxScaler,
+    MaxAbsScaler,
+    RobustScaler,
+    Normalizer,
+    QuantileTransformer,
+    PowerTransformer
+)
