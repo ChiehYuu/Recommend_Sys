889ba82145dba68f7ddc2dfb541319328727ba8c
Jay <jj830502@gmail.com>
2023-09-20 21:08:27 +0800

    main build

--- AlgoSys/main.py
+++ AlgoSys/main.py
@@ -1,3 +1,23 @@
-import pyyaml
+import yaml
+from 
 
-path = "./config.yaml"
+def load_config(config_path):
+    with open(config_path, 'r') as stream:
+        config = yaml.safe_load(stream)
+    return config
+
+def main():
+    config = load_config('config.yaml')
+
+    model_names = config['model_names']
+    hyperparameters = config['hyperparameters']
+    data = ...  # Load your data here
+
+    mixer = RegressionModelMixer(model_names, hyperparameters, data)
+
+    # Assuming you have some test data X_test
+    result = mixer.get_ensemble_result(X_test)
+    print(result)
+
+if __name__ == "__main__":
+    main()
